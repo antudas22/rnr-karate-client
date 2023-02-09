81b52ca159131e063e41aa040c9fc910327e4d75
antudas22 <20antu20@gmail.com>
2023-02-09 19:24:24 +0600

    show all students in manage students page

--- src/Pages/Dashboard/ManageStudents/ManageStudents.js
+++ src/Pages/Dashboard/ManageStudents/ManageStudents.js
@@ -1,11 +1,68 @@
-import React from 'react';
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import Loading from "../../Shared/Loading/Loading";
 
 const ManageStudents = () => {
-    return (
-        <div>
-            <h3 className='text-2xl font-bold my-4'>Manage Students</h3>
-        </div>
-    );
+  const { data: students, isLoading } = useQuery({
+    queryKey: ["students"],
+    queryFn: async () => {
+      try {
+        const res = await fetch("http://localhost:5000/students", {
+          headers: {
+            authorization: `bearer ${localStorage.getItem("accessToken")}`,
+          },
+        });
+        const data = await res.json();
+        return data;
+      } catch (error) {}
+    },
+  });
+
+  if(isLoading){
+    return <Loading />
+  }
+
+  return (
+    <div>
+      <h3 className="text-2xl font-bold my-4">
+        Manage Students: {students?.length}
+      </h3>
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Avatar</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {students?.map((student, i) => (
+              <tr key={student._id} className="hover">
+                <th>{i + 1}</th>
+                <td>
+                  <div className="avatar">
+                    <div className="w-16 rounded-full">
+                      <img src={student.image} alt="avatar" />
+                    </div>
+                  </div>
+                </td>
+                <td>{student.name}</td>
+                <td>{student.email}</td>
+                <td>{student.phone}</td>
+                <td>
+                  <button className="btn btn-xs bg-red-600">Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default ManageStudents;
